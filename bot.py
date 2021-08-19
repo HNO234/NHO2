2ed00ea17e26183137ac0cc5d3794db6913d9046
HNO234 <dave910602@gmail.com>
2021-08-20 00:06:03 +0800

    fix init commit

--- bot.py
+++ bot.py
@@ -1,5 +1,4 @@
 import discord
-import secret
 import crawler
 import notcommand
 import os
@@ -18,13 +17,14 @@ def get_class(command):
 async def on_message(message):
     if message.author == client.user:
         return
+
+    # For testing purpose
+    if str(message.author.id) != os.getenv('AUTHOR_ID'):
+        return
+
     if message.content.strip() == 'nho2':
         await message.channel.send("Hello! This is Discord Bot developed by HNO2.\nYou can find my source code at https://github.com/HNO234/NHO2.")
     if not message.content.startswith('nho2 '):
-        return
-
-    # For testing purpose
-    if message.author.id != os.getenv('AUTHOR_ID'):
         return
 
     arguments = BotArguments(message.content.strip().split()[1:])
--- crawler.py
+++ crawler.py
@@ -1,5 +1,4 @@
 import requests
-import secret
 import asyncio
 import discord
 import os
